c5e85b69aa839f1c459d3f6b3764e6ca3b665270
William Huchim Ricalde <williamhuchim@gmail.com>
2021-06-29 13:09:33 -0500

    style: :sparkles: Linkedin button
    Linkedin buttons added

--- components/ContactButtons/index.js
+++ components/ContactButtons/index.js
@@ -6,7 +6,7 @@ const ContactButtons = () => {
     <div className={style.container}>
       <a href='mailto:williamhuchim@gmail.com' className={style.button}><FaEnvelope /></a>
       <a href='https://github.com/WilliamRicalde' className={style.button}><FaGithub /></a>
-      <a href='https://github.com/WilliamRicalde' className={style.button}><FaLinkedinIn /></a>
+      <a href='https://linkedin.com/in/william-huchim' className={style.button}><FaLinkedinIn /></a>
     </div>
   )
 }
